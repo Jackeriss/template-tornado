a07b9b38159ea9bde863a44ddc80e0ad853b2585
yufeizhao <yufeizhao@clubfactory.com>
2020-01-16 20:18:12 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const gulp = require('gulp');
 const cp = require("child_process")
 
+gulp.task('install', function () {
+  return cp.exec('pipenv install --deploy')
+})
+
 gulp.task('start', function () {
   return cp.exec('/usr/local/bin/pm2 startOrReload pm2.json')
 })
 
-exports.default = gulp.series('start')
+exports.default = gulp.series('install', 'start')
