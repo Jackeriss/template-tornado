202257a92ca5dba71674cdfabc6d631076155160
yufeizhao <yufeizhao@clubfactory.com>
2020-01-16 15:20:35 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require('gulp');
 const cp = require("child_process")
 
-gulp.task('deploy', function() {
+gulp.task('start', function () {
   return cp.exec('/usr/local/bin/pm2 startOrReload pm2.json')
+})
+
+gulp.task('default', function () {
+  gulp.start('start')
 })
