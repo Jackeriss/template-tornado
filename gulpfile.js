323f1259504ca60b4dfa39e3332001cf13b0031c
yufeizhao <yufeizhao@clubfactory.com>
2021-03-03 20:15:15 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 const gulp = require('gulp');
 const run = require('gulp-run')
 
-function install() {
-  return run('pipenv install --deploy').exec()
-}
-
 function start() {
   return run('/usr/local/bin/pm2 startOrReload pm2.json').exec()
 }
